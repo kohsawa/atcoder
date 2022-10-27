58853da12f5dbb2ed5f527a438a0d72d599dc0c0
blacktr1007 <ohs1192kou@gmail.com>
2022-10-27 23:09:18 +0900

    Solve: until chapter 1.3

--- tessoku-book/a03/main.cpp
+++ tessoku-book/a03/main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    int N, K, P[100], Q[100];
+    bool flag = false;
 
+    cin >> N >> K;
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+        P[i] = K - P[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> Q[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (P[i] == Q[j]) {
+                flag = true;
+                break;
+            }
+        }
+    }
+
+    if (flag == true) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
 }
--- tessoku-book/b03/main.cpp
+++ tessoku-book/b03/main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    int N, A[1000];
+    bool flag = false;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N - 2; i++) {
+        for (int j = i + 1; j < N - 1; j++) {
+            for (int k = j + 1; k < N; k++) {
+                if (A[i] + A[j] + A[k] == 1000) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (flag == true) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
 
 }
