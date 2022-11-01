67a336346c0f8a4bc5c2529f97cdef556b3bac37
blacktr1007 <ohs1192kou@gmail.com>
2022-11-01 23:49:38 +0900

    Solve: until chapter 2.1

--- tessoku-book/a06/main.cpp
+++ tessoku-book/a06/main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    int N, Q;
+    int A[100001], S[100001] = { 0 };
+    int L, R;
 
+    cin >> N >> Q;
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        S[i] = S[i - 1] + A[i];
+    }
+    for (int i = 1; i <= Q; i++) {
+        cin >> L >> R;
+        cout << S[R] - S[L - 1] << endl;
+    }
+    return 0;
 }
--- tessoku-book/b06/main.cpp
+++ tessoku-book/b06/main.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    int N, Q, A[100001], L, R, S[100001] = { 0 };
 
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        S[i] = S[i - 1] + A[i];
+    }
+
+    cin >> Q;
+    for (int i = 0; i < Q; i++) {
+        cin >> L >> R;
+        int d = R - L + 1;
+        int win = S[R] - S[L - 1];
+
+        if (win * 2 > d) cout << "win" << endl;
+        else if (win * 2 < d) cout << "lose" << endl;
+        else cout << "draw" << endl;
+    }
+    return 0;
 }
